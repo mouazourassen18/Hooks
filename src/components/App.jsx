0e717c0fa3e17d131083b958db64d8a52fd22be7
mouazourassen18 <54610297+mouazourassen18@users.noreply.github.com>
2021-08-29 14:57:59 -0400

    final_update_Hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div />;
+  const [count, setCount] = useState(0);
+
+  function increaseC() {
+    setCount(count + 1);
+  }
+  function decreaseC() {
+    setCount(count - 1);
+  }
+  return (
+    <div className="container">
+      <h1 style={{ color: "green" }}>{count}</h1>
+      <button onClick={increaseC}>+</button>
+      <button onClick={decreaseC}>-</button>
+    </div>
+  );
 }
 
 export default App;
